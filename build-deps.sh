2bd170b73b85f28d6c77418bcf2474b71e4fe7ee
Tinson Lai <laitingsheng@hotmail.com>
2022-08-12 15:10:00 +0800

    Allow custom CMake generator

--- build-deps.sh
+++ build-deps.sh
@@ -2,12 +2,13 @@
 set -eu
 
 install_prefix="${2}"
+generator="${3:-Ninja}"
 
 mkdir -p "${install_prefix}"
 
 ort_path="$(realpath ${install_prefix}/ort)"
 
-cmake --toolchain "${PWD}/toolchains/${1}.cmake" -G Ninja -B build/ort \
+cmake --toolchain "${PWD}/toolchains/${1}.cmake" -G "${generator}" -B build/ort \
 	-DCMAKE_INSTALL_PREFIX="$ort_path" \
 	-Donnxruntime_BUILD_SHARED_LIB=ON \
 	-Donnxruntime_USE_CUDA=ON \
@@ -24,7 +25,7 @@ cmake --build build/ort --target install
 
 torch_path="$(realpath ${install_prefix}/torch)"
 
-cmake --toolchain "${PWD}/toolchains/${1}.cmake" -G Ninja -B build/torch \
+cmake --toolchain "${PWD}/toolchains/${1}.cmake" -G "${generator}" -B build/torch \
 	-DCMAKE_INSTALL_PREFIX="$torch_path" \
 	-DATEN_NO_TEST=ON \
 	-DBUILD_PYTHON=OFF \
@@ -44,7 +45,7 @@ cmake --build build/torch --target install
 
 torchvision_path="$(realpath ${install_prefix}/torchvision)"
 
-cmake --toolchain "${PWD}/toolchains/${1}.cmake" -G Ninja -B build/torchvision \
+cmake --toolchain "${PWD}/toolchains/${1}.cmake" -G "${generator}" -B build/torchvision \
 	-DCMAKE_INSTALL_PREFIX="$torchvision_path" \
 	-DCMAKE_PREFIX_PATH="$torch_path" \
 	-DWITH_CUDA=ON \
